e610dca3243756770db11be6a7ef26583c6fb1f5
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-09-18 01:43:54 +0300

    Update game.js

--- Web_Ass2/game.js
+++ Web_Ass2/game.js
@@ -5,14 +5,11 @@ let hover; //variable used to check if the user hovered over S to start counting
 let end; //where the game ends 
 let stat; //display the status of the user (won or lost)
 let score = 0; //keep track of the score
-let store;
-let border;
-let hover_e = true;
-let i;
+let store; //put score inside the element that has id = boundary example
+let hover_e = true; //keep track of the hovers over E 
+let i; //counter
 
 window.onload = function(){
-    // border = document.getElementById("game");
-    // border.onmouseout = overBoundary;
     start = document.getElementById("start"); //access the element that has id = start
     start.onmouseover = hover_start; //check if the mouse hovered over S to start the game
     start.onclick = sClick; //check if the user click on S ti reset settings
@@ -27,6 +24,7 @@ window.onload = function(){
     
 }
 
+//If the user touches the boundaries
 function overBoundary(){
     lost = true;
 
@@ -43,18 +41,14 @@ function overBoundary(){
             hover_e = false;
         }
     }
-    //this loop add a class to the boundaries without removing/affecting existing classes
-    // for (let i = 0; i < bounds.length - 1; i++) {
-    //     bounds[i].className += " youlose";
-    // }
-    // stat.textContent = "YOU LOST!!"
-    
 }
 
+//Start the game when we hover over S
 function hover_start(){
     hover = true;
 }
 
+//refresh/restart when clicking on S
 function sClick(){
     hover_e = true;
     lost = false;
@@ -71,6 +65,7 @@ function sClick(){
     stat.textContent = "Begin by moving your mouse over the \"S\".";
 }
 
+//Check if the user won when hover over the E
 function end_game(){
     //stat = document.getElementById("status");
     if(!lost){
