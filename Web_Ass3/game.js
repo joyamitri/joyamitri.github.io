566159e9590e5491c10db15ea1549bd8a8d7e6b3
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-10-02 17:21:18 +0300

    Updates

--- Web_Ass3/game.js
+++ Web_Ass3/game.js
@@ -1,110 +1,106 @@
-const row1 = document.querySelectorAll("div.row1 > div.cell");
-const row2 = document.querySelectorAll("div.row2 > div.cell");
-const row3 = document.querySelectorAll("div.row3 > div.cell");
+let origBoard;
+const human_play = "red";
+const ai_player = "yellow";
+const win_comb = [["row1-cell1", "row1-cell2", "row1-cell3"], 
+["row2-cell1", "row2-cell2", "row2-cell3"], 
+["row3-cell1", "row3-cell2", "row3-cell3"],
+["row1-cell1", "row2-cell1", "row3-cell1"],
+["row1-cell2", "row2-cell2", "row3-cell2"],
+["row1-cell3", "row2-cell3", "row3-cell3"],
+["row1-cell1", "row2-cell2", "row3-cell3"],
+["row1-cell3", "row2-cell2", "row3-cell1"]];
+const occupied = [-1, -1, -1, -1, -1, -1, -1, -1];
 
-function myMoveR1(x, y){
+const cells = document.querySelectorAll(".cell");
+const start = document.getElementById("start");
+start.addEventListener("click", startGame);
+
+function startGame() {
+    document.getElementById("sett").style.display = "none";
+    origBoard = Array.from(Array(9).keys());
+    for(let a in cells){
+        cells[a].onclick = function(){turnClick(a)};
+    }
+}
+
+// a.onclick = function (){turn(150, a.id)};
+
+function turnClick(a) {
+    if (typeof origBoard[a] == 'number') {
+        turn(150, cells[a].id);
+        // if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
+    }
+}
+
+function turn(cell_pos, cell_id){
     const el = document.getElementById("coin");
     let pos = 0;
     let id = null;
     clearInterval(id);
     id = setInterval(frame, 5);
     function frame(){
-        if(pos == 150){
+        if(pos == cell_pos){
         clearInterval(id);
         }else{
             pos++;
-            if(y == "row1-cell1"){
-                el.style.top = pos + "px";
-                el.style.right = 150 + "px";
+            if(cell_id == "row1-cell1"){
+                el.style.top = 150 + "px";
+                el.style.right = cell_pos + "px";
                 el.style.opacity = 1; 
+                
             }
-            else if(y == "row1-cell2"){
-                el.style.top = pos + "px";
-                el.style.bottom = pos + "px";
+            else if(cell_id == "row1-cell2"){
+                el.style.top = 150 + "px";
+                el.style.bottom = cell_pos + "px";
                 el.style.opacity = 1; 
+                
             }
-            else if(y == "row1-cell3"){
-                el.style.top = pos + "px";
-                el.style.left = 150 + "px";
+            else if(cell_id == "row1-cell3"){
+                el.style.top = 150 + "px";
+                el.style.left = cell_pos + "px";
                 el.style.opacity = 1; 
+                
+            }
+            else if(cell_id == "row2-cell1"){
+                el.style.top = 310 + "px";
+                el.style.right = cell_pos + "px";
+                el.style.opacity = 1; 
+                
+            }
+            else if(cell_id == "row2-cell2"){
+                el.style.top = 310 + "px";
+                el.style.bottom = cell_pos + "px";
+                el.style.opacity = 1; 
+                
+            }
+            else if(cell_id == "row2-cell3"){
+                el.style.top = 310 + "px";
+                el.style.left = cell_pos + "px";
+                el.style.opacity = 1; 
+                
+            }
+            else if(cell_id == "row3-cell1"){
+                el.style.top = 460 + "px";
+                el.style.right = cell_pos + "px";
+                el.style.opacity = 1; 
+                
+            }
+            else if(cell_id == "row3-cell2"){
+                el.style.top = 460 + "px";
+                el.style.bottom = cell_pos + "px";
+                el.style.opacity = 1; 
+                
+            }
+            else if(cell_id == "row3-cell3"){
+                el.style.top = 460 + "px";
+                el.style.left = cell_pos + "px";
+                el.style.opacity = 1; 
+                
             }
             
         }
     }
-}
 
-function myMoveR2(x, y){
-    const el = document.getElementById("red");
-    let pos = 0;
-    let id = null;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame(){
-        if(pos == x){
-        clearInterval(id);
-        }else{
-            pos++;
-            if(y == "row2-cell1"){
-                el.style.top = pos + "px";
-                el.style.right = 150 + "px";
-                el.style.opacity = 1; 
-            }
-            else if(y == "row2-cell2"){
-                el.style.top = pos + "px";
-                el.style.bottom = pos + "px";
-                el.style.opacity = 1; 
-            }
-            else if(y == "row2-cell3"){
-                el.style.top = pos + "px";
-                el.style.left = 150 + "px";
-                el.style.opacity = 1; 
-            }
-            
-        }
-    }
-}
-
-function myMoveR3(x, y){
-    const el = document.getElementById("red");
-    let pos = 0;
-    let id = null;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame(){
-        if(pos == x){
-        clearInterval(id);
-        }else{
-            pos++;
-            if(y == "row3-cell1"){
-                el.style.top = pos + "px";
-                el.style.right = 150 + "px";
-                el.style.opacity = 1; 
-            }
-            else if(y == "row3-cell2"){
-                el.style.top = pos + "px";
-                el.style.bottom = pos + "px";
-                el.style.opacity = 1; 
-            }
-            else if(y == "row3-cell3"){
-                el.style.top = pos + "px";
-                el.style.left = 150 + "px";
-                el.style.opacity = 1; 
-            }
-            
-        }
-    }
-}
-
-for(let a of row1){
-    a.onclick = function(){myMoveR1(150, a.id)};
-}
-
-for(let b of row2){
-    b.onclick = function(){myMoveR2(310, b.id)};
-}
-
-for(let c of row3){
-    c.onclick = function(){myMoveR3(460, c.id)};
 }
 
 
